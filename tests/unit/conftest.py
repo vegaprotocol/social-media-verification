a0ae5690b0e5ab1ee0be97724c4a4adc1420dba0
fkondej <fkondej@gmail.com>
2021-04-07 00:46:05 +0200

    Further linter issues

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,17 +1,18 @@
 # This file is automatically picked up and run by pytest
 # It defines special marker: `@pytest.mark.focus`
-import pytest
+
 
 def pytest_collection_modifyitems(items):
-    '''If any items are marked 'focus', run only those items.
+    """If any items are marked 'focus', run only those items.
 
     This hook is run automatically by pytest.
-    '''
+    """
     # Find items with the 'focus' mark
     focused = [item for item in items if item.get_closest_marker("focus")]
     # If any have it, replace the full list of collected items with only them
     if focused:
         items[:] = focused
+
 
 def pytest_configure(config):
     config.addinivalue_line(
